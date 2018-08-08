c14cadf10478f29c5aae59defdaec3ae0cd8cbf9
ramborell23 <jerelldavis@ac.c4q.nyc>
2018-08-08 11:28:49 -0400

    Complete update od DB. Now contains every fight, fighter, tournament, championship

--- db/prideFC.sql
+++ db/prideFC.sql
@@ -39,7 +39,7 @@ COPY Fights
 (event_id,weight_class,fighter_1,fighter_2,winner ,method ,"round" ,"time", notes) 
 FROM '/Users/c4q/Documents/prideFCApi/db/Fights.csv' DELIMITER ',' CSV HEADER;
 
-
+Drop TABLE Fighters;
 CREATE TABLE Fighters
 (
     id SERIAL PRIMARY KEY,
@@ -53,35 +53,45 @@ CREATE TABLE Fighters
     record INT,
     wins INT,
     loss INT,
-    draw INT,
-    wko INT,
-    wsubmission INT,
-    wdecision INT,
-    lkO INT,
-    lsubmission INT,
-    ldecision INT,
-    notes VARCHAR
+    draw INT
 );
--- COPY Fights
--- (debut_year,name,alias,"from",fighting_out_of,height ,weight ,record ,wins, loss,draw,wko,wsubmission,wdecision,
--- lkO,lsubmission,ldecision,notes) 
--- FROM '/Users/c4q/Documents/prideFCApi/db/Fighters.csv' DELIMITER ',' CSV HEADER;
+COPY Fighters
+(debut_year,name,alias,"from",fighting_out_of,height,weight,wins,loss,draw) 
+FROM '/Users/c4q/Documents/prideFCApi/db/Fighters.csv' DELIMITER ',' CSV HEADER;
 
 
+Drop TABLE Title_Holders;
 CREATE TABLE Title_Holders
 (
     name VARCHAR NOT NULL,
     division VARCHAR NOT NULL,
-    date DATE,
-    no_defenses INT
-
+    event_title_won INT NOT NULL,
+    event_title_loss INT,
+    num_defenses INT,
+    FOREIGN Key(event_title_won) REFERENCES Events(id),
+    FOREIGN Key(event_title_loss) REFERENCES Events(id)
 );
+COPY Title_Holders
+(name,division,event_title_won,event_title_loss,num_defenses) 
+FROM '/Users/c4q/Documents/prideFCApi/db/TitleHolders.csv' DELIMITER ',' CSV HEADER;
 
+
+Drop TABLE Tournaments;
 CREATE TABLE Tournaments
 (
-    event_id INT,
+    year INT,
     division VARCHAR NOT NULL,
-    winner VARCHAR,
+    champion VARCHAR,
     runner_up VARCHAR,
-    FOREIGN Key(event_id) REFERENCES Events(id)
+    final_eventid INT,
+    semis_eventid INT,
+    quarter_eventid INT,
+    opening_eventid INT,
+    FOREIGN Key(final_eventid) REFERENCES Events(id),
+    FOREIGN Key(semis_eventid) REFERENCES Events(id),
+    FOREIGN Key(quarter_eventid) REFERENCES Events(id),
+    FOREIGN Key(opening_eventid) REFERENCES Events(id)
 );
+COPY Tournaments 
+(year,division,champion,runner_up,final_eventid,semis_eventid,quarter_eventid,opening_eventid) 
+FROM '/Users/c4q/Documents/prideFCApi/db/Tournaments.csv' DELIMITER ',' CSV HEADER;
